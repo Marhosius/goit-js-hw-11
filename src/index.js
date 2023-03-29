d5fab6d4c6ceded0da5e4b5c5b19c541a7720687
Marhosius <marhosius@gmail.com>
2023-03-30 01:55:34 +0300

    Load more function

--- src/index.js
+++ src/index.js
@@ -7,48 +7,56 @@ import { PixabayApi } from './js/fetchPhoto';
 const formEl = document.querySelector('.search-form');
 const gallaryDiv = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+loadMoreBtn.hidden = true;
 
 const pixabayApi = new PixabayApi();
+let inputValue = '';
+let gallery = new simpleLightbox('.gallery a', {
+    captionsData: "title"
+});
 
 const formListener = formEl.addEventListener('submit', onFormSubmit)
+const loadMoreListener = loadMoreBtn.addEventListener('click', onLoadMore)
 
 async function onFormSubmit(ev) {
     ev.preventDefault();
     gallaryDiv.innerHTML = '';
-    let inputValue = ev.currentTarget.elements[0].value;
+    inputValue = ev.currentTarget.elements[0].value;
     let subBtn = ev.currentTarget.elements[1];
 
     if (!inputValue.trim()) {
         console.log(`Empty`)
         return
     }
-
-    await createGallary(inputValue);
-    let gallery = new simpleLightbox('.gallery a', {
-        captionsData: "title"
-    });
-
+    loadMoreBtn.hidden = false;
+    const request = await createGallary(inputValue, 1);
+    gallery.refresh()
 };
 
 
-async function createGallary(searchRequest) {
+async function createGallary(searchRequest, page) {
     pixabayApi.searchValue = searchRequest;
+    pixabayApi.currentPage = page;
     const ApiRes = await pixabayApi.getPhoto();
     const markup = ApiRes.data.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
 
     <a rel="noopener noreferrer" class="gallery__link" href="${largeImageURL}">
-        <div class="photo-card">
             <img class="gallery__image" src="${webformatURL}" data-source="${largeImageURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-        <p class="info-item"><b>Likes:${likes}</b></p>
-        <p class="info-item"><b>Views:${views}</b></p>
-        <p class="info-item"><b>Comments:${comments}</b></p>
-        <p class="info-item"><b>Downloads:${downloads}</b></p>
-            </div>
+        <div class="info">
+            <p class="info-item"><b>Likes: ${likes}</b></p>
+            <p class="info-item"><b>Views: ${views}</b></p>
+            <p class="info-item"><b>Comments: ${comments}</b></p>
+            <p class="info-item"><b>Downloads: ${downloads}</b></p>
         </div>
     </a>
     `);
     gallaryDiv.insertAdjacentHTML('beforeEnd', markup.join(""));
+    return ApiRes;
 }
 
+async function onLoadMore(el) {
+    ++pixabayApi.currentPage
+    const request = await createGallary(inputValue, pixabayApi.currentPage);
+    console.log(!request.data.hits.length)
+}
 
--- src/js/fetchPhoto.js
+++ src/js/fetchPhoto.js
@@ -7,10 +7,10 @@ export class PixabayApi {
     #API_KEY = `34827210-dfbfa3fa5ea498ab11e20bf55`;
     #BASE_URL = 'https://pixabay.com/api/';
     #SEARCH_TYPE = `photo`;
-    #SAFE_SEARCH = false;
+    #SAFE_SEARCH = true;
     currentPage = 1;
     perPage = 40;
-    searchValue = `cars`;
+    searchValue = ``;
 
     getPhoto = () => axios.get(`${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.searchValue}&image_type=${this.#SEARCH_TYPE}&page=${this.currentPage}&per_page=${this.perPage}&orientation=horizontal&safesearch=${this.#SAFE_SEARCH}`);
 
