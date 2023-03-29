aabc1e168118819136669fc913c7204042803015
Marhosius <marhosius@gmail.com>
2023-03-29 18:13:54 +0300

    functions for gallary

--- src/js/fetchPhoto.js
+++ src/js/fetchPhoto.js
@@ -1,15 +1,17 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
-const API_KEY = `34827210-dfbfa3fa5ea498ab11e20bf55`;
-const BASE_URL = 'https://pixabay.com/api/';
-const SEARCH_TYPE = `photo`;
-const SAFE_SEARCH = false;
-let currentPage = 1;
-let perPage = 40;
-let searchValue = `cars`;
 
-const getPhoto = () => axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchValue}&image_type=${SEARCH_TYPE}&page=${currentPage}&per_page=${perPage}&orientation=horizontal&safesearch=${SAFE_SEARCH}`);
 
-getPhoto().then(({ data }) => {
-    console.log(data);
-}).catch(console.warn);
+export class PixabayApi {
+
+    #API_KEY = `34827210-dfbfa3fa5ea498ab11e20bf55`;
+    #BASE_URL = 'https://pixabay.com/api/';
+    #SEARCH_TYPE = `photo`;
+    #SAFE_SEARCH = false;
+    currentPage = 1;
+    perPage = 40;
+    searchValue = `cars`;
+
+    getPhoto = () => axios.get(`${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.searchValue}&image_type=${this.#SEARCH_TYPE}&page=${this.currentPage}&per_page=${this.perPage}&orientation=horizontal&safesearch=${this.#SAFE_SEARCH}`);
+
+}
